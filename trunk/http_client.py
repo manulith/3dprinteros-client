649b9394924128dad56ba11330f7d7243af7729f
vladimir <vladimir@itbsg.eu>
2015-01-05 13:27:12 +0200

    Test soft for server programmers.

--- trunk/http_client.py
+++ trunk/http_client.py
@@ -54,19 +54,19 @@ def package_users_login(username, password, error=[None,None]):
     user_hash = md5_hash(username)
     pass_hash = md5_hash(password)
     data = { 'login': {'user': user_hash, 'password': pass_hash}, 'error': error}
-    return json.dump(data), user_login_path
+    return json.dumps(data), user_login_path
 
 def package_printer_login(login_hash, printer_profile, error=[None,None]):
     data = { 'login_hash': login_hash, 'printer': printer_profile, 'error': error }
-    return json.dump(data), printer_login_path
+    return json.dumps(data), printer_login_path
 
 def package_command_request(printer_session_hash, state, error=[None,None]):
     data = { 'printer_session_hash': printer_session_hash, 'state': state, 'error': error }
-    return json.dump(data), command_path
+    return json.dumps(data), command_path
 
 def package_cloud_sync_upload(token, file_data, file_name):
     data = { 'token': token, 'file_data': file_data, 'file_name': file_name }
-    return json.dump(data), cloudsync_path
+    return json.dumps(data), cloudsync_path
 
 #TODO turn on https
 def connect(URL):
@@ -111,6 +111,7 @@ def send(packager, payloads):
     if type(payloads) != tuple:
         payloads = [ payloads ]
     connection = connect(URL)
+    print payloads
     if connection:
         request_body, path = packager(*payloads)
         json_answer = post_request(connection, request_body, path)
@@ -143,35 +144,57 @@ def multipart_upload(url, token, file_obj):
 
 if __name__ == '__main__':
     import command_processor
+    import printer_interface
 
     user = ''
     password = ''
-    profile = '[{"extruder_count": 1, "baudrate": [250000, 115200], "vids_pids": [["0403", "6001"], ["4745", "0001"],\
-        ["2341", "0042"]], "name": "Ultimaker 2", "VID": "2341", "PID": "0042", \
-         "end_gcodes": ["M107", "M104 S0", "M140 S0", "G91", "G1 E-5 F300", "G28 X0 Y0 Z0", "M84", "G90", "M25"],\
-          "driver": "printrun_printer", "reconnect_on_cancel": true, "Product": null, "SNR": null,\
-           "COM": "/dev/ttyACM0", "Manufacturer": null, "force_port_close": true, "print_from_binary": false},\
-            {"extruder_count": 1, "baudrate": [250000, 115200], "vids_pids": [["16C0", "0483"], ["2341", "0042"]],\
-             "name": "Marlin Firmware", "VID": "2341", "PID": "0042", "end_gcodes": [], "driver": "printrun_printer",\
-              "reconnect_on_cancel": false, "Product": null, "SNR": null, "COM": "/dev/ttyACM0", "Manufacturer": null,\
-               "force_port_close": true, "print_from_binary": false}]'
+    profile = json.loads('{"extruder_count": 1, "baudrate": [250000, 115200], "vids_pids": [["16C0", "0483"], ["2341", "0042"]], "name": "Marlin Firmware", "VID": "2341", "PID": "0042", "end_gcodes": [], "driver": "printrun_printer", "reconnect_on_cancel": false, "Product": null, "SNR": null, "COM": "/dev/ttyACM0", "Manufacturer": null, "force_port_close": false, "print_from_binary": false}')
+    #2pr_int = printer_interface.PrinterInterface(profile)
     while True:
+        user = "Nobody"
+        password = "qwert"
+        user_login_hash = ""
+        printer_login_hash = ""
         user_choice = raw_input('Welcome to test menu:\n' \
                                 'Type 1 for - User login\n' \
                                 'Type 2 for - Printer login\n' \
                                 'Type 3 for - Command request\n')
         if  '1' in user_choice:
-            answer = send(package_users_login, user, password)
-            processor = command_processor.process_user_login
+            answer = send(package_users_login, (user, password))
+            if answer:
+                processor = command_processor.process_user_login
+                result = processor(answer)
+                user_login_hash = result
+            else:
+                print 'No answer'
         elif '2' in user_choice:
-            answer = send(package_users_login, profile)
-            processor = command_processor.process_printer_login
+            if not user_login_hash:
+                print "!First you need to login as user"
+            else:
+                answer = send(package_printer_login, (user_login_hash, profile))
+                if answer:
+                    processor = command_processor.process_printer_login
+                    result = processor(answer)
+                    printer_login_hash = result
+                else:
+                    print 'No answer'
         elif '3' in user_choice:
-            answer = send(package_users_login, profile)
-            processor = command_processor.process_command_request
+            if not printer_login_hash:
+                print "!First you need to login printer"
+            else:
+                answer = send(package_command_request, printer_login_hash)
+                if answer:
+                    processor = command_processor.process_command_request
+                    result = processor(user_choice)
+                else:
+                    print 'No answer'
         else:
-            answer =  'Invalid choice'
-        print user_choice
-        print 'Raw answer:' + str(answer)
-        print command_processor
+            print 'Invalid choice'
+
+        try:
+            print 'Raw answer: ' + str(answer)
+            print 'Processed answer: ' + str(result)
+        except:
+            pass
+
 
