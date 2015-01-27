948dc812391bb0dee9bc3dd4cae78247ecf0856f
vladimir <vladimir@itbsg.eu>
2015-01-27 16:52:45 +0200

    Updated URLs and paths

--- trunk/http_client.py
+++ trunk/http_client.py
@@ -12,18 +12,16 @@ CONNECTION_TIMEOUT = 6
 URL = config.config['URL']
 AUX_URL = config.config['AUX_URL']
 HTTPS_FLAG = config.config['HTTPS']
-user_login_path = "/user_login"
-printer_login_path = "/printer_login"
-command_path = "/command"
+streamer_prefix = "/streamerapi"
+user_login_path = streamer_prefix + "/user_login"
+printer_login_path = streamer_prefix + "/printer_login"
+command_path = streamer_prefix + "/command"
 cloudsync_path = "/autoupload"
-token_jobs_path = "/getJobs"
-token_login_path = "/sendRequestToken" #json['token': token]
+
 token_camera_path = "/oldliveview/setLiveView" #json['image': base64_image ]
 token_send_logs_path = "/oldliveview/sendLogs"
 
 domain_path_re = re.compile("https?:\/\/(.+)(\/.*)")
-
-
 
 def load_json(jdata):
     logger = logging.getLogger('app.' +__name__)
