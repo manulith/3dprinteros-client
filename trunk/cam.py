784fca157ee0f8a4f56b5bd10e9ff563f17d667c
Oleg <panasevychol@gmail.com>
2015-01-29 10:49:14 +0200

    Cosmetic fix in camera logging.

--- trunk/cam.py
+++ trunk/cam.py
@@ -127,7 +127,7 @@ class CameraImageSender(threading.Thread):
         #data = {"user_token": self.token, "camera_number": self.camera_number, "camera_name": self.camera_name, "data": picture, "host_mac": http_client.MACADDR}
         #http_client.multipart_upload(self.url, data)
         answer =  http_client.send(http_client.package_camera_send, (self.token, self.camera_number, self.camera_name, picture, http_client.MACADDR))
-        self.logger.info('Camera streaming response: ' + str(answer))
+        self.logger.info(self.camera_name + ' streaming response: ' + str(answer))
 
     def close(self):
         self.stop_flag = True
