072e6732b5baea00f9dfd871a518cb832cd5727c
alexey <alexey@itbsg.eu>
2015-02-02 16:00:36 +0200

    camera subprocess

--- trunk/app.py
+++ trunk/app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 import time
 import signal
 import logging
@@ -36,7 +37,21 @@ class App:
         self.init_interface()
         self.user_login.wait_for_login()
         if config.config["camera"]["enabled"] == True:
-            self.cam = Popen(['python', 'cam.py'], stdout=PIPE, stderr=PIPE)
+            self.logger.info('Launching camera subprocess')
+            client_dir = os.path.dirname(os.path.abspath(__file__))
+            cam_path = os.path.join(client_dir, 'cam.py')
+            assert os.path.isfile(cam_path)
+            try:
+                self.cam = Popen(['python', cam_path], stdout=PIPE, stderr=PIPE)
+            except:
+                self.logger.info('Python is not installed in system. \
+                Using our portable interpreter for camera subprocess.')
+                python_path = os.path.abspath(os.path.join(client_dir, '..', 'python27', 'python.exe'))
+                assert os.path.isfile(python_path)
+                try:
+                    self.cam = Popen([python_path, cam_path], stdout=PIPE, stderr=PIPE)
+                except Exception as e:
+                    self.logger.warning('Could not launch camera with our interpreter due to:\n' + e.message)
         self.main_loop()
 
 
