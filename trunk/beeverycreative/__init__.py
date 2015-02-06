297c5413bfe8fce7262f5aa889693e30236483a7
vladimir <vladimir@itbsg.eu>
2015-02-06 14:05:00 +0200

    Fixed for beeverycreative3

--- trunk/beeverycreative/__init__.py
+++ trunk/beeverycreative/__init__.py
@@ -18,7 +18,7 @@ class Sender(printrun_printer.Sender):
         self.usb_info = usb_info
         self.logger = logging.getLogger("app." + __name__)
         if usb_info.get('COM', None):
-            printrun_printer.Printer.__init__(self, profile, usb_info)
+            printrun_printer.Sender.__init__(self, profile, usb_info)
         else:
             if self.init_raw_usb_device():
                 self.flash_firmware()
