7e592b4a87c35210a7f713dffe19a762e7d172f3
vladimir <vladimir@itbsg.eu>
2015-02-17 14:15:08 +0200

    Fixed wrong position of error field in message in printer_interface2

--- trunk/printer_interface.py
+++ trunk/printer_interface.py
@@ -127,7 +127,7 @@ class PrinterInterface(threading.Thread):
             report = self.state_report()
             self.report = report # for web_interface
             message = [self.printer_token, report, self.acknowledge, self.sender_error]
-            if not message[3] and self.printer:
+            if not message[3] and self.printer and self.printer.error_code:
                 message[3] = {"code": self.printer.error_code, "message": self.printer.error_message}
             self.logger.debug("Requesting with: %s" % str(message))
             if self.printer.is_operational():
