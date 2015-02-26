305df1f7b88622231b354576379d9a5336f437f3
Oleg <panasevychol@gmail.com>
2015-02-26 14:39:11 +0200

    Cosmetic mark

--- trunk/web_interface.py
+++ trunk/web_interface.py
@@ -83,7 +83,8 @@ class WebInterfaceHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 printers_list.append(printer)
             printers = ''.join(map(lambda x: "<p>" + x + "</p>", printers_list))
             if not printers:
-                printers = '<p><b>No printers detected</b><br>Please do a power cycle for pritners\
+                printers = '<p><b>No printers detected</b>\
+                <br>Please do a power cycle for pritners\
                 <br>and then ensure your printers are connected\
                 <br>to power outlet and usb cord</p>'
             page = page.replace('!!!PRINTERS!!!', printers)
