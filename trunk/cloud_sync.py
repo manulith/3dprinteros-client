aed365d58561035427d462344e3689a1ef94639e
Oleg <panasevychol@gmail.com>
2015-04-07 14:24:05 +0300

    Fix for create_shortcuts

--- trunk/cloud_sync.py
+++ trunk/cloud_sync.py
@@ -160,7 +160,7 @@ class Cloudsync:
         self.logger.info('Cloudsync started!')
         self.create_folders()
         if self.os == 'windows':
-            #self.create_shortcuts_win()
+            self.create_shortcuts_win()
             self.enable_disk_label()
         while not self.stop_flag:
             try:
