c0b4159046ad086c60f4af54e893c672883cc7b9
Oleg <panasevychol@gmail.com>
2015-04-20 11:47:32 +0300

    Small mark: launching subprocess function moved to utils

--- trunk/utils.py
+++ trunk/utils.py
@@ -27,14 +27,15 @@ LOG_SNAPSHOTS_DIR = 'log_snapshots'
 LOG_SNAPSHOT_LINES = 200
 
 
-def launch_suprocess(self, file_name):
-    self.logger.info('Launching as subprocess ' + file_name)
+def launch_suprocess(file_name):
+    logger = logging.getLogger('app')
+    logger.info('Launching as subprocess ' + file_name)
     client_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(client_dir, file_name)
     try:
         process = Popen([sys.executable, path])
     except Exception as e:
-        self.logger.warning('Could not launch ' + file_name + ' as subprocess due to error:\n' + e.message)
+        logger.warning('Could not launch ' + file_name + ' as subprocess due to error:\n' + e.message)
     else:
         return process
 
