ef9c7743f8794f41555e98caf40f9e69a05c901c
vladimir <vladimir@itbsg.eu>
2015-04-28 16:42:21 +0300

    Updated release_notes and removed unneeded files.

--- trunk/zmorph_sender.py
+++
@@ -1,7 +0,0 @@
-import smoothie_sender
-
-class Sender(smoothie_sender.Sender):
-    def __init__(self, profile, usb_info, app):
-        smoothie_sender.Sender.__init__(self, profile, usb_info, app)
-
-
