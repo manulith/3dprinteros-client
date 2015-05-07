cdb2c34e47c2829c49fc240514ea29504aba515e
Oleg <o.panasevych@itbsg.eu>
2015-05-07 12:00:39 +0300

    Fix for installer scripts

--- tools/installer/build.bat
+++ tools/installer/build.bat
@@ -1,3 +1,4 @@
 echo
-echo python get_version.py | (set /p VERSION= & set VERSION)
-makensis /DVERSION="%VERSION%" /DBUILD="%1" 3dprinteros-client.nsi
+python get_version.py
+set /p version= < version.txt
+makensis /DVERSION="%version%" /DBUILD="%1" 3dprinteros-client.nsi
