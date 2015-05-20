5d35ab41fe651d05dcba50e35c5c588dc2fe5d0c
Oleg <panasevychol@gmail.com>
2015-05-20 17:35:29 +0300

    Signed myself as author in few modules

--- client/cloud_sync.py
+++ client/cloud_sync.py
@@ -16,7 +16,7 @@
 #You should have received a copy of the GNU Affero General Public License
 #along with 3DPrinterOS client.  If not, see <http://www.gnu.org/licenses/>.
 
-# Author: Vladimir Avdeev <another.vic@yandex.ru> 2015
+# Author: Oleg Panasevych <panasevychol@gmail.com>
 
 import sys
 import os
